a6430b5f74ea3a678c1bbf8d9f080da95bc88a70
johnlinvc <johnlinvc@gmail.com>
2020-09-01 13:22:52 +0800

    Initial commit

---
+++ lib/acmesmith-azure-dns-zones/version.rb
@@ -0,0 +1,3 @@
+module AcmesmithAzureDnsZones
+  VERSION = "0.1.0"
+end
---
+++ spec/acmesmith-azure-dns-zones/zones_spec.rb
@@ -0,0 +1,6 @@
+RSpec.describe AcmesmithAzureDnsZones do
+  it "has a version number" do
+    expect(AcmesmithAzureDnsZones::VERSION).not_to be nil
+  end
+
+end
